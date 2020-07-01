cdf932d5c38697edbb5d36ea2a4217230b74ddda
youngjin <teen14y@gmail.com>
2020-07-01 14:01:54 +0900

    add drop fucntionality

--- src/components/BoardSquare.js
+++ src/components/BoardSquare.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useDrop } from "react-dnd";
+
 import Square from "components/Square";
+import Overlay from "components/Overlay";
+import { ItemTypes } from "constants/index";
+import { moveKnight, canMoveKnight } from "data/Game";
 
 export default function BoardSquare({ x, y, children }) {
   const black = (x + y) % 2 === 1;
-  return <Square black={black}>{children}</Square>;
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: ItemTypes.KNIGHT,
+    drop: () => moveKnight(x, y),
+    canDrop: () => canMoveKnight(x, y),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  });
+  return (
+    <div
+      ref={drop}
+      style={{ position: "relative", width: "100%", height: "100%" }}
+    >
+      <Square black={black}>{children}</Square>
+      {isOver && canDrop && <Overlay color="green" />}
+      {!isOver && canDrop && <Overlay color="yellow" />}
+      {isOver && !canDrop && <Overlay color="red" />}
+    </div>
+  );
 }
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,27 +1,23 @@
 import React from "react";
 
-import Square from "components/Square";
 import Knight from "components/Knight";
-import { moveKnight, canMoveKnight } from "data/Game";
+import BoardSquare from "components/BoardSquare";
 
-export const renderSquare = (i, [knightX, knightY]) => {
+export const renderSquare = (i, knightPosition) => {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const black = (x + y) % 2 === 1;
-  const isKightHere = knightX === x && knightY === y;
-  const piece = isKightHere ? <Knight /> : null;
-  const handlerSquareClick = (toX, toY) => {
-    if (canMoveKnight(toX, toY)) {
-      moveKnight(toX, toY);
-    }
-  };
 
   return (
-    <div
-      style={{ width: "12.5%", height: "12.5%" }}
-      onClick={() => handlerSquareClick(x, y)}
-    >
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
+};
+
+const renderPiece = (x, y, [knightX, knightY]) => {
+  if (knightX === x && knightY === y) {
+    return <Knight />;
+  }
 };
